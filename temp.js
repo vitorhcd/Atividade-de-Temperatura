bf0a2f24571a073e7ea5928c37db7d453f29eb6e
Vitor <vitor_h_andrade@estudante.sesisenai.org.br>
2024-02-07 16:48:10 -0300

    Ultima Versão

--- temp.js
+++ temp.js
@@ -1,4 +1,5 @@
 let resp = document.getElementById("resp")
+let resp2 = document.getElementById("resp2")
 
 function calc(){
     let calc = 0
@@ -7,4 +8,13 @@ function calc(){
     calc = (temp * 9/5) + 32
 
     resp.innerHTML = "Sua temperatura em Farenheit é = " + calc
+}
+function calc2(){
+    let temp2 = Number(document.getElementById("temp2").value)
+    let calc3 = 0
+
+    calc3 = (temp2 - 32) * 5/9
+    
+    resp2.innerHTML = "Sua temperatura em Celsius é = " + calc3
+ 
 }
